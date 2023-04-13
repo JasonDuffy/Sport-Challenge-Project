178f35ee87f660db805572c805670b6646815f97
Jason Patrick Duffy <jaduit00@hs-esslingen.de>
2023-04-13 15:12:38 +0200

    feat: Basisprojekt für die Implementierung erstellt
    Refs: SCP-104
    Time: 1.0 h

---
+++ Backend/src/test/java/de/hsesslingen/scpprojekt/scp/ScpApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsesslingen.scpprojekt.scp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

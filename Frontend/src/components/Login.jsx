85eb44f4ddf7ba1136d8b44e83c91732a5db99f2
Jason Patrick Duffy <jaduit00@hs-esslingen.de>
2023-04-27 13:44:06 +0200

    fix: Datenbankdaten werden jetzt lokal gespeichert
    Refs: -
    Time: -

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import Button from "./Button";
+import { useState, useEffect } from "react";
 
 const userAction = async () => {
-    const response = await fetch('http://localhost:8081/saml/user');
+    const res = await fetch("http://localhost:8081/saml/user");
+    console.log(res);
+    const data = await res.json();
+    console.log(data);
 }
 
 function Login(){

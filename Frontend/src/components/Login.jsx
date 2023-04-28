0b4ab2a4e75420a177fda15f7de7b388b9040de7
Jason Patrick Duffy <jaduit00@hs-esslingen.de>
2023-04-28 20:27:48 +0200

    fix: Link zur Test Rest API gefixt, DB erstellt sich nicht mehr neu
    Refs: SCP-2, SCP-102, SCP-106
    Time: 0.1 h

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 import { useState, useEffect } from "react";
 
 const userAction = async () => {
-    const res = await fetch("http://localhost:8081/saml/user/", {method:"GET", credentials: "include"});
+    const res = await fetch("http://localhost:8081/saml/user/", { method:"GET", credentials: "include" });
     console.log(res);
     const data = await res.json();
     console.log(data);
@@ -12,7 +12,7 @@ const userAction2 = () => {
     window.open("http://localhost:8081/saml/login/", "_self")
 }
 const userAction3 = async () => {
-    const res = await fetch("http://localhost:8081/api/members/0", { method: "GET", credentials: "include" });
+    const res = await fetch("http://localhost:8081/member/0", { method: "GET", credentials: "include" });
     console.log(res);
     const data = await res.json();
     console.log(data);

1a223f1216f856dd26d233b02a0cc2ad5116fd77
Robin Hackh <rohait02@hs-esslingen.de>
2023-05-07 13:36:08 +0200

    feat: Add Challenge wird nun dynamisch generiert
    Refs: SCP-116
    Time: 1.5 h

--- Frontend/src/components/AddChallenge.jsx
+++ Frontend/src/components/AddChallenge.jsx
@@ -1,157 +1,185 @@
-import React from "react";
+import React, { Component } from "react";
 import { useState } from "react";
 import Button from "./Button";
 import "./css/AddChallenge.css";
 
 /**
  * Add Challenge page of the App
- * 
+ *
  * @author Robin Hackh
  */
-function AddChallenge() {
-  const [challengeName, setChallengeName] = useState("");
-  const [challengeImage, setChallengeImage] = useState("");
-  const [challengeDescription, setChallengeDescription] = useState("");
-  const [challengeDistanceGoal, setChallengeDistanceGoal] = useState("");
-  const [challengeStartDate, setChallengeStartDate] = useState("");
-  const [challengeEndDate, setChallengeEndDate] = useState("");
+class AddChallenge extends Component {
+  constructor() {
+    super();
+    this.state = {
+      challengeName: "",
+      challengeDescription: "",
+      challengeDistanceGoal: 0,
+      challengeStartDate: "",
+      challengeEndDate: "",
+      allSport: []
+    };
 
-  function challengeNameChange(event) {
-    setChallengeName(event.target.value);
+    this.challengeNameChange = this.challengeNameChange.bind(this);
+    this.challengeDescriptionChange = this.challengeDescriptionChange.bind(this);
+    this.challengeDistanceGoalChange = this.challengeDistanceGoalChange.bind(this);
+    this.challengeStartDateChange = this.challengeStartDateChange.bind(this);
+    this.challengeEndDateChange = this.challengeEndDateChange.bind(this);
+    this.submitHandle = this.submitHandle.bind(this);
   }
 
-  function challengeImageChange(event) {
-    setChallengeImage(event.target.value);
+  challengeNameChange(event){
+    this.setState({challengeName: event.target.value});
   }
 
-  function challengeDescriptionChange(event) {
-    setChallengeDescription(event.target.value);
+  challengeDescriptionChange(event){
+    this.setState({challengeDescription: event.target.value});
+  }
+  challengeDistanceGoalChange(event){
+    this.setState({challengeDistanceGoal: event.target.value});
+  }
+  challengeStartDateChange(event){
+    this.setState({challengeStartDate: event.target.value});
+  }
+  challengeEndDateChange(event){
+    this.setState({challengeEndDate: event.target.value});
   }
 
-  function challengeDistanceGoalChange(event) {
-    setChallengeDistanceGoal(event.target.value);
+  async submitHandle(event) {
+    event.preventDefault();
+    let response = await fetch("http://localhost:8081/sport/all/", { method: "GET", credentials: "include" });
+    let resData = await response.json();
+    console.log(resData);
+    const challengeImageEl = document.getElementById("challenge_image");
+    const sportCheckboxEl = document.getElementsByClassName("form_sport_checkbox");
+  
+    for (const iterator of sportCheckboxEl) {
+      console.log("value: " + iterator.checked + "  id: " + iterator.dataset.sportId);
+    }
+
+    console.log("Name " + this.state.challengeName);
+    console.log("Iamge " + challengeImageEl.files[0].size);
+    console.log("Description " + this.state.challengeDescription);
+    console.log("Distance Goal " + this.state.challengeDistanceGoal);
+    console.log("Start date " + this.state.challengeStartDate);
+    console.log("End date " + this.state.challengeEndDate);
   }
 
-  function challengeStartDateChange(event) {
-    setChallengeStartDate(event.target.value);
+  async componentDidMount(){
+    let response = await fetch("http://localhost:8081/sport/all/", { method: "GET", credentials: "include" });
+    let resData = await response.json();
+    this.setState({allSport: resData});
   }
 
-  function challengeEndDateChange(event) {
-    setChallengeEndDate(event.target.value);
-  }
-
-  return (
-    <section className="background_white">
-      <div className="section_container">
-        <div className="section_content">
-          <div className="heading_underline_center mg_b_10">
-            <span className="underline_center">Challenge hinzufügen</span>
-          </div>
-          <div className="form_container">
-            <form>
-              <div className="form_input_container pd_1">
-                <h2>Gib der Challenge einen Namen</h2>
-                <input
-                  className="mg_t_2"
-                  type="text"
-                  value={challengeName}
-                  onChange={challengeNameChange}
-                  placeholder="Challenge Name"
-                ></input>
-              </div>
-              <div className="form_input_container pd_1 mg_t_2">
-                <h2>Wähle ein Bild für deine Challenge</h2>
-                <span className="form_input_description">
-                  Das Bild repräsentiert deine Challenge auf der Startseite.
-                  <br />
-                  <br />
-                  Das Bild sollte quadratisch sein.
-                </span>
-                <br />
-                <input className="mg_t_2" type="file" value={challengeImage} onChange={challengeImageChange}></input>
-              </div>
-              <div className="form_input_container pd_1 mg_t_2">
-                <h2>Beschreibe deine Challenge</h2>
-                <div className="form_input_description_content">
-                  <textarea
+  render() {
+    return (
+      <section className="background_white">
+        <div className="section_container">
+          <div className="section_content">
+            <div className="heading_underline_center mg_b_10">
+              <span className="underline_center">Challenge hinzufügen</span>
+            </div>
+            <div className="form_container">
+              <form onSubmit={this.submitHandle}>
+                <div className="form_input_container pd_1">
+                  <h2>Gib der Challenge einen Namen</h2>
+                  <input
                     className="mg_t_2"
-                    type="textArea"
-                    value={challengeDescription}
-                    onChange={challengeDescriptionChange}
-                    placeholder="Beschreibe deine Challenge"
-                  ></textarea>
+                    type="text"
+                    value={this.state.challengeName}
+                    onChange={this.challengeNameChange}
+                    placeholder="Challenge Name"
+                  ></input>
                 </div>
-              </div>
-              <div className="form_input_container pd_1 mg_t_2">
-                <h2>Definiere ein Ziel für deine Challenge</h2>
-                <span className="form_input_description">Gib hier die Kilometerzahl ein, die erreicht werden soll</span>
-                <br />
-                <input
-                  className="mg_t_2"
-                  type="number"
-                  min={0}
-                  value={challengeDistanceGoal}
-                  onChange={challengeDistanceGoalChange}
-                  placeholder="Kilometer"
-                ></input>
-              </div>
-              <div className="form_input_container pd_1 mg_t_2">
-                <h2>Wähle die Sportarten aus, die an der Challenge teilnehmen dürfen</h2>
-                <table className="form_sport_table mg_t_2">
-                  <thead>
-                    <tr>
-                      <th>Sportart</th>
-                      <th>Multiplier</th>
-                      <th>Select</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>Fahrrad</td>
-                      <td>0</td>
-                      <td>
-                        <input className="form_table_checkbox" type="checkbox"></input>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>Laufen</td>
-                      <td>3</td>
-                      <td>
-                        <input className="form_table_checkbox" type="checkbox"></input>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <div className="form_input_container pd_1 mg_t_2">
-                <h2>Wähle das Start- und Enddatum für deine Challenge</h2>
-                <div className="form_input_date_container mg_t_2">
-                  <div className="form_input_date_content">
-                    <input
-                      id="form_input_start_date"
-                      type="datetime-local"
-                      value={challengeStartDate}
-                      onChange={challengeStartDateChange}
-                    ></input>
-                    <span className="form_input_date_separator mg_x_1">----</span>
-                    <input
-                      id="form_input_end_date"
-                      type="datetime-local"
-                      value={challengeEndDate}
-                      onChange={challengeEndDateChange}
-                    ></input>
+                <div className="form_input_container pd_1 mg_t_2">
+                  <h2>Wähle ein Bild für deine Challenge</h2>
+                  <span className="form_input_description">
+                    Das Bild repräsentiert deine Challenge auf der Startseite.
+                    <br />
+                    <br />
+                    Das Bild sollte quadratisch sein.
+                  </span>
+                  <br />
+                  <input id="challenge_image" className="mg_t_2" type="file"></input>
+                </div>
+                <div className="form_input_container pd_1 mg_t_2">
+                  <h2>Beschreibe deine Challenge</h2>
+                  <div className="form_input_description_content">
+                    <textarea
+                      className="mg_t_2"
+                      type="textArea"
+                      value={this.state.challengeDescription}
+                      onChange={this.challengeDescriptionChange}
+                      placeholder="Beschreibe deine Challenge"
+                    ></textarea>
                   </div>
                 </div>
-              </div>
-              <div className="center_content mg_t_2">
-                <Button color="orange" txt="Challenge erstellen" type="submit"/>
-              </div>
-            </form>
+                <div className="form_input_container pd_1 mg_t_2">
+                  <h2>Definiere ein Ziel für deine Challenge</h2>
+                  <span className="form_input_description">Gib hier die Kilometerzahl ein, die erreicht werden soll</span>
+                  <br />
+                  <input
+                    className="mg_t_2"
+                    type="number"
+                    min={0}
+                    value={this.state.challengeDistanceGoal}
+                    onChange={this.challengeDistanceGoalChange}
+                    placeholder="Kilometer"
+                  ></input>
+                </div>
+                <div className="form_input_container pd_1 mg_t_2">
+                  <h2>Wähle die Sportarten aus, die an der Challenge teilnehmen dürfen</h2>
+                  <table className="form_sport_table mg_t_2">
+                    <thead>
+                      <tr>
+                        <th>Sportart</th>
+                        <th>Multiplier</th>
+                        <th>Select</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {this.state.allSport.map(item => (
+                      <tr key={item.id}>
+                      <td>{item.name}</td>
+                      <td>{item.factor}</td>
+                      <td>
+                        <input className="form_table_checkbox form_sport_checkbox" data-sport-id={item.id} type="checkbox"></input>
+                      </td>
+                    </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+                <div className="form_input_container pd_1 mg_t_2">
+                  <h2>Wähle das Start- und Enddatum für deine Challenge</h2>
+                  <div className="form_input_date_container mg_t_2">
+                    <div className="form_input_date_content">
+                      <input
+                        id="form_input_start_date"
+                        type="datetime-local"
+                        value={this.state.challengeStartDate}
+                        onChange={this.challengeStartDateChange}
+                      ></input>
+                      <span className="form_input_date_separator mg_x_1">----</span>
+                      <input
+                        id="form_input_end_date"
+                        type="datetime-local"
+                        value={this.state.challengeEndDate}
+                        onChange={this.challengeEndDateChange}
+                      ></input>
+                    </div>
+                  </div>
+                </div>
+                <div className="center_content mg_t_2">
+                  <Button color="orange" txt="Challenge erstellen" type="submit" />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
 
 export default AddChallenge;

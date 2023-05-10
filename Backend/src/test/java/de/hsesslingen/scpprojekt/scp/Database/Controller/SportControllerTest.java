d9a3a471438806dee9b1f9f5fc18d5d4575be166
Tom Nguyen Dinh <tongit00@hs-essslingen.de>
2023-05-10 15:20:52 +0200

    feat: Test für Sport und Member fertig  TeamMember  angefangen
    Time: 3h

---
+++ Backend/src/test/java/de/hsesslingen/scpprojekt/scp/Database/Controller/SportControllerTest.java
@@ -0,0 +1,425 @@
+package de.hsesslingen.scpprojekt.scp.Database.Controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.hsesslingen.scpprojekt.scp.Database.Entities.Sport;
+import de.hsesslingen.scpprojekt.scp.Database.Repositories.SportRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.client.HttpServerErrorException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ *  SportController Tests
+ *
+ * @author Tom Nguyen Dinh
+ */
+@ActiveProfiles("test")
+@WebMvcTest(SportController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class SportControllerTest {
+
+    @MockBean
+    private SportRepository sportRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     *Test for Successfully creating a Sport
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithMockUser
+    public void addSportSuccess()throws Exception{
+        Sport sport = new Sport();
+        sport.setId(1);
+        sport.setName("Laufen");
+        sport.setFactor(10);
+
+        when(sportRepository.save(any(Sport.class))).thenReturn(sport);
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/sports/")
+                .content(new ObjectMapper().writeValueAsString(sport))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isCreated())
+                .andReturn();
+        String content = res.getResponse().getContentAsString();
+
+        Pattern pattern = Pattern.compile("\\{\"id\":(\\d),");
+        Matcher matcher = pattern.matcher(content);
+
+        matcher.find();
+        assertEquals(matcher.group(1), "1");
+        assertFalse(matcher.find());
+
+        Mockito.verify(sportRepository).save(any(Sport.class));
+    }
+
+    /**
+     * Test for InternalServerError
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithMockUser
+    public void addSportSomethingWentWrong()throws Exception{
+        Sport sport  = new Sport();
+
+        when(sportRepository.save(any(Sport.class))).thenThrow(HttpServerErrorException.InternalServerError.class);
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/sports/")
+                .content(new ObjectMapper().writeValueAsString(sport))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isInternalServerError())
+                .andReturn();
+
+    }
+    /**
+     *Test for  creating a Sport with not been login
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithAnonymousUser
+    public void addSportNogLogin()throws Exception{
+        Sport sport = new Sport();
+        sport.setId(1);
+        sport.setName("Laufen");
+        sport.setFactor(10);
+
+        when(sportRepository.save(any(Sport.class))).thenReturn(sport);
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/sports/")
+                .content(new ObjectMapper().writeValueAsString(sport))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isForbidden())
+                .andReturn();
+    }
+
+    /**
+     *Test for Successfully searching a Sport
+     * @throws Exception Exception by mockMvc
+     */
+
+    @Test
+    @WithMockUser
+    public void getSportByIDSuccess() throws Exception{
+        Sport sport = new Sport();
+        sport.setId(1L);
+        sport.setName("Laufen");
+        sport.setFactor(10);
+
+        when(sportRepository.findById(1L)).thenReturn(Optional.of(sport));
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/sports/1/").accept(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = res.getResponse().getContentAsString();
+
+        Pattern pattern = Pattern.compile("\\{\"id\":(\\d),");
+        Matcher matcher = pattern.matcher(content);
+
+        matcher.find();
+        assertEquals(matcher.group(1), "1");
+        assertFalse(matcher.find());
+
+        Mockito.verify(sportRepository).findById(1L);
+    }
+
+    /**
+     *Test for searching a non-existing Sport (Not Found)
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithMockUser
+    public void getSportByIDNotFound() throws Exception{
+        Sport sport = new Sport();
+        sport.setId(1L);
+        sport.setName("Laufen");
+        sport.setFactor(10);
+
+        when(sportRepository.findById(1L)).thenReturn(Optional.of(sport));
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/sports/4/").accept(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+    /**
+     * Test if unknown user is correctly turned away
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithAnonymousUser
+    public void getSportByIDLogout() throws Exception{
+        Sport sport = new Sport();
+        sport.setId(1L);
+        sport.setName("Laufen");
+        sport.setFactor(10);
+
+        when(sportRepository.findById(1L)).thenReturn(Optional.of(sport));
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/sports/1/").accept(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isForbidden())
+                .andReturn();
+    }
+
+    /**
+     *Test for searching all Sports  with Success
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithMockUser
+    public void getAllSportSuccess() throws Exception{
+        Sport sport = new Sport();
+        sport.setId(1L);
+        sport.setName("Laufen");
+        sport.setFactor(10);
+
+        Sport Rad = new Sport();
+        Rad.setId(2L);
+        Rad.setName("Rad");
+        Rad.setFactor(10);
+        List<Sport> sportsList = new ArrayList<>();
+        sportsList.add(sport); sportsList.add(Rad);
+
+        when(sportRepository.findAll()).thenReturn(sportsList);
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/sports/").accept(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = res.getResponse().getContentAsString();
+
+        Pattern pattern = Pattern.compile("\\{\"id\":(\\d),");
+        Matcher matcher = pattern.matcher(content);
+
+        matcher.find();
+        assertEquals(matcher.group(1), "1");
+        matcher.find();
+        assertEquals(matcher.group(1), "2");
+        assertFalse(matcher.find());
+
+        Mockito.verify(sportRepository).findAll();
+    }
+
+    /**
+     * Test if unknown user is correctly turned away
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithAnonymousUser
+    public void getAllSportLogout() throws Exception{
+        Sport sport = new Sport();
+        sport.setId(1L);
+        sport.setName("Laufen");
+        sport.setFactor(10);
+
+        Sport Rad = new Sport();
+        Rad.setId(2L);
+        Rad.setName("Rad");
+        Rad.setFactor(10);
+        List<Sport> sportsList = new ArrayList<>();
+        sportsList.add(sport); sportsList.add(Rad);
+
+        when(sportRepository.findAll()).thenReturn(sportsList);
+        RequestBuilder request = MockMvcRequestBuilders
+                .get("/sports/").accept(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isForbidden())
+                .andReturn();
+    }
+    /**
+     *Test for deleting a Sport with Success
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithMockUser
+    public void DeleteSportByIdSuccess() throws Exception{
+        Sport sport = new Sport();
+        sport.setId(1L);
+        sport.setName("Laufen");
+        sport.setFactor(10);
+
+        when(sportRepository.findById(1L)).thenReturn(Optional.of(sport));
+        RequestBuilder request = MockMvcRequestBuilders
+                .delete("/sports/1/").accept(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().is(204))
+                .andReturn();
+
+        Mockito.verify(sportRepository).findById(1L);
+        Mockito.verify(sportRepository).deleteById(1L);
+    }
+
+    /**
+     *Test for deleting a non-existing Sport (Not Found)
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithMockUser
+    public void DeleteSportByIdNotFound() throws Exception{
+        Sport sport = new Sport();
+        sport.setId(1L);
+        sport.setName("Laufen");
+        sport.setFactor(10);
+
+        when(sportRepository.findById(1L)).thenReturn(Optional.of(sport));
+        RequestBuilder request = MockMvcRequestBuilders
+                .delete("/sports/3/").accept(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isNotFound())
+                .andReturn();
+    }
+    /**
+     *Test if unknown User is correctly turned away
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithAnonymousUser
+    public void DeleteSportByIdLogOut() throws Exception{
+        Sport sport = new Sport();
+        sport.setId(1L);
+        sport.setName("Laufen");
+        sport.setFactor(10);
+
+        when(sportRepository.findById(1L)).thenReturn(Optional.of(sport));
+        RequestBuilder request = MockMvcRequestBuilders
+                .delete("/sports/1/").accept(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isForbidden())
+                .andReturn();
+
+    }
+    /**
+     *Test for updating a sport with success
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithMockUser
+    public void UpdateSportSuccess()throws Exception{
+        Sport sport = new Sport();
+        sport.setId(1L);
+        sport.setName("Laufen");
+        sport.setFactor(10);
+
+        when(sportRepository.findById(1L)).thenReturn(Optional.of(sport));
+        when(sportRepository.save(any(Sport.class))).thenReturn(sport);
+        RequestBuilder request = MockMvcRequestBuilders
+                .put("/sports/1/")
+                .content(new ObjectMapper().writeValueAsString(sport))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String content = res.getResponse().getContentAsString();
+
+        Pattern pattern = Pattern.compile("\\{\"id\":(\\d),");
+        Matcher matcher = pattern.matcher(content);
+
+        matcher.find();
+        assertEquals(matcher.group(1), "1");
+        assertFalse(matcher.find());
+
+        Mockito.verify(sportRepository).findById(1L);
+        Mockito.verify(sportRepository).save(sport);
+    }
+    /**
+     *Test for updating a non-existing Sport (Not Found)
+     * @throws Exception Exception by mockMvc
+     */
+
+    @Test
+    @WithMockUser
+    public void UpdateSportNotFound()throws Exception{
+        Sport sport = new Sport();
+        sport.setId(1L);
+        sport.setName("Laufen");
+        sport.setFactor(10);
+
+        when(sportRepository.findById(1L)).thenReturn(Optional.of(sport));
+        when(sportRepository.save(any(Sport.class))).thenReturn(sport);
+        RequestBuilder request = MockMvcRequestBuilders
+                .put("/sports/4/")
+                .content(new ObjectMapper().writeValueAsString(sport))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+    }
+
+    /**
+     * Test for turning unknown User away
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithAnonymousUser
+    public void UpdateSportLogOut()throws Exception{
+        Sport sport = new Sport();
+        sport.setId(1L);
+        sport.setName("Laufen");
+        sport.setFactor(10);
+
+        when(sportRepository.findById(1L)).thenReturn(Optional.of(sport));
+        when(sportRepository.save(any(Sport.class))).thenReturn(sport);
+        RequestBuilder request = MockMvcRequestBuilders
+                .put("/sports/1/")
+                .content(new ObjectMapper().writeValueAsString(sport))
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isForbidden())
+                .andReturn();
+
+    }
+
+}
---
+++ Backend/src/test/java/de/hsesslingen/scpprojekt/scp/Database/Controller/TeamMemberControllerTest.java
@@ -0,0 +1,309 @@
+package de.hsesslingen.scpprojekt.scp.Database.Controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.hsesslingen.scpprojekt.scp.Database.Entities.Member;
+import de.hsesslingen.scpprojekt.scp.Database.Entities.Team;
+import de.hsesslingen.scpprojekt.scp.Database.Entities.TeamMember;
+import de.hsesslingen.scpprojekt.scp.Database.Repositories.MemberRepository;
+import de.hsesslingen.scpprojekt.scp.Database.Repositories.TeamMemberRepository;
+import de.hsesslingen.scpprojekt.scp.Database.Repositories.TeamRepository;
+import org.apache.coyote.Request;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ *  TeamMemberController Tests
+ *
+ * @author Tom Nguyen Dinh
+ */
+@ActiveProfiles("test")
+@WebMvcTest(TeamMemberController.class)
+@AutoConfigureMockMvc(addFilters = false)
+public class TeamMemberControllerTest {
+
+    @MockBean
+    TeamRepository teamRepository;
+    @MockBean
+    MemberRepository memberRepository;
+    @MockBean
+    TeamMemberRepository teamMemberRepository;
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * test for successful adding a Member to a Team
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithMockUser
+    public void addMemberToTeamSuccess()throws Exception{
+        Member member = new Member();
+        member.setId(3);
+        member.setFirstName("Max");
+        member.setLastName("Mustermann");
+
+        Team team = new Team();
+        team.setId(2);
+        team.setName("red Nidhogg");
+
+        TeamMember teamMember = new TeamMember();
+        teamMember.setId(1);
+        teamMember.setTeam(team);
+        teamMember.setMember(member);
+
+        when(memberRepository.findById(3L)).thenReturn(Optional.of(member));
+        when(teamRepository.findById(2L)).thenReturn(Optional.of(team));
+        when(teamMemberRepository.save(any(TeamMember.class))).thenReturn(teamMember);
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/teamMembers/").accept(MediaType.APPLICATION_JSON)
+                .param("TeamID", "2")
+                .param("MemberID", "3");
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isCreated())
+                .andReturn();
+        String content = res.getResponse().getContentAsString();
+
+        Pattern pattern = Pattern.compile("\\{\"id\":(\\d),");
+        Matcher matcher = pattern.matcher(content);
+
+        matcher.find();
+        assertEquals(matcher.group(1), "1");
+        matcher.find();
+        assertEquals(matcher.group(1), "2");
+        matcher.find();
+        assertEquals(matcher.group(1), "3");
+        assertFalse(matcher.find());
+
+        Mockito.verify(memberRepository).findById(3L);
+        Mockito.verify(teamRepository).findById(2L);
+        Mockito.verify(teamMemberRepository).save(any(TeamMember.class));
+    }
+
+    /**
+     * test by adding a Member to a non-existing Team
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithMockUser
+    public void addMemberToTeamNotFound1()throws Exception{
+        Member member = new Member();
+        member.setId(3);
+        member.setFirstName("Max");
+        member.setLastName("Mustermann");
+
+        Team team = new Team();
+        team.setId(2);
+        team.setName("red Nidhogg");
+
+        TeamMember teamMember = new TeamMember();
+        teamMember.setId(1);
+        teamMember.setTeam(team);
+        teamMember.setMember(member);
+
+        when(memberRepository.findById(3L)).thenReturn(Optional.of(member));
+        when(teamMemberRepository.save(any(TeamMember.class))).thenReturn(teamMember);
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/teamMembers/").accept(MediaType.APPLICATION_JSON)
+                .param("TeamID", "2")
+                .param("MemberID", "3");
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+        Mockito.verify(memberRepository).findById(3L);
+
+    }
+
+    /**
+     * test by adding non-existing Member to Team
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithMockUser
+    public void addMemberToTeamNotFound2()throws Exception{
+        Member member = new Member();
+        member.setId(3);
+        member.setFirstName("Max");
+        member.setLastName("Mustermann");
+
+        Team team = new Team();
+        team.setId(2);
+        team.setName("red Nidhogg");
+
+        TeamMember teamMember = new TeamMember();
+        teamMember.setId(1);
+        teamMember.setTeam(team);
+        teamMember.setMember(member);
+
+        when(teamRepository.findById(2L)).thenReturn(Optional.of(team));
+        when(teamMemberRepository.save(any(TeamMember.class))).thenReturn(teamMember);
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/teamMembers/").accept(MediaType.APPLICATION_JSON)
+                .param("TeamID", "2")
+                .param("MemberID", "3");
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+        Mockito.verify(teamRepository).findById(2L);
+
+    }
+
+    /**
+     * test if unknown User is thrown
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithAnonymousUser
+    public void addMemberToTeamLogOut()throws Exception{
+        Member member = new Member();
+        member.setId(3);
+        member.setFirstName("Max");
+        member.setLastName("Mustermann");
+
+        Team team = new Team();
+        team.setId(2);
+        team.setName("red Nidhogg");
+
+        TeamMember teamMember = new TeamMember();
+        teamMember.setId(1);
+        teamMember.setTeam(team);
+        teamMember.setMember(member);
+
+        when(memberRepository.findById(3L)).thenReturn(Optional.of(member));
+        when(teamRepository.findById(2L)).thenReturn(Optional.of(team));
+        when(teamMemberRepository.save(any(TeamMember.class))).thenReturn(teamMember);
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .post("/teamMembers/").accept(MediaType.APPLICATION_JSON)
+                .param("TeamID", "2")
+                .param("MemberID", "3");
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isForbidden())
+                .andReturn();
+    }
+
+    /**
+     * test for deleting TeamMember
+     * @throws Exception Exception by mockMvc
+     */
+    @Test
+    @WithMockUser
+    public void deleteMemberofTeamSuccess()throws Exception{
+        Member member = new Member();
+        member.setId(3);
+        member.setFirstName("Max");
+        member.setLastName("Mustermann");
+
+        Team team = new Team();
+        team.setId(2);
+        team.setName("red Nidhogg");
+
+        TeamMember teamMember = new TeamMember();
+        teamMember.setId(1);
+        teamMember.setTeam(team);
+        teamMember.setMember(member);
+
+        when(teamMemberRepository.findById(1L)).thenReturn(Optional.of(teamMember));
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .delete("/teamMembers/1/").accept(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Mockito.verify(teamMemberRepository).findById(1L);
+        Mockito.verify(teamMemberRepository).deleteById(1L);
+    }
+
+    @Test
+    @WithAnonymousUser
+    public void deleteMemberofTeamLogout()throws Exception{
+        Member member = new Member();
+        member.setId(3);
+        member.setFirstName("Max");
+        member.setLastName("Mustermann");
+
+        Team team = new Team();
+        team.setId(2);
+        team.setName("red Nidhogg");
+
+        TeamMember teamMember = new TeamMember();
+        teamMember.setId(1);
+        teamMember.setTeam(team);
+        teamMember.setMember(member);
+
+
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .delete("/teamMembers/1/").accept(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isForbidden())
+                .andReturn();
+
+
+    }
+
+    @Test
+    @WithMockUser
+    public void deleteMemberofTeamNotFound()throws Exception{
+        Member member = new Member();
+        member.setId(3);
+        member.setFirstName("Max");
+        member.setLastName("Mustermann");
+
+        Team team = new Team();
+        team.setId(2);
+        team.setName("red Nidhogg");
+
+        TeamMember teamMember = new TeamMember();
+        teamMember.setId(1);
+        teamMember.setTeam(team);
+        teamMember.setMember(member);
+
+        RequestBuilder request = MockMvcRequestBuilders
+                .delete("/teamMembers/1/").accept(MediaType.APPLICATION_JSON);
+
+        MvcResult res = mockMvc.perform(request)
+                .andExpect(status().isNotFound())
+                .andReturn();
+
+        Mockito.verify(teamMemberRepository).findById(1L);
+    }
+
+
+}

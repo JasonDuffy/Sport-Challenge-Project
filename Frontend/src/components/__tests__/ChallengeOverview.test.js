aa1b63c22b1abcc2ec7074896a495fcf67670034
Robin Hackh <rohait02@hs-esslingen.de>
2023-05-11 19:30:33 +0200

    test: Mock für Tests vorbereitet
    Refs: -
    Time: 0.5 h

--- Frontend/src/components/__tests__/ChallengeOverview.test.js
+++ Frontend/src/components/__tests__/ChallengeOverview.test.js
@@ -1,41 +1,18 @@
-import ChallengeOverview from "../ChallengeOverview";
-import { render, getChallengeData } from "@testing-library/react";
-
-
-/*
-test('fetch data from backend', async() => {
-  const mockData = {cha: 'challenge'};
-  const mockResponse = {
-    ok: true,
-    json: jest.fn().mockResolvedValue(mockData)
-  };
-  jest.spyOn(global, 'fetch').mockResolvedValue(mockResponse);
-  
-  const result = await getChallengeData();
-  expect(result).toEqual(mockData);
-});
-
-global.fetch = jest.fn(() =>
-    Promise.resolve({
-        json: () => Promise.resolve()
-    })
-)
-
+/*import ChallengeOverview from "../ChallengeOverview";
 
 beforeEach(() => {
-    fetch.mockClear();
+  fetch.resetMocks();
 });
-*/
 
-describe('challenge overview', () => {
-  beforeEach(() => {
-    fetch.resetMocks()
-  })
-  
-  it('testing something...', () => {
-    fetch.mockResponseOnce(JSON.stringify({ data: '12345' }))
-    const container = render (<ChallengeOverview/>)
-    // expect statement can be wrong.
-    expect(container.getByText(/12345/).toBeDefined())
-  })
-});
+it('Return score', async () => {
+  fetch.mockResponseOnce(JSON.stringify({
+    result: [
+      {
+        user: 'John Doe',
+        score: 42,
+      }],
+  }));
+  const res = await ChallengeOverview.getChallengeData();
+  expect(res).toEqual([{ score: 42, user: 'John Doe' }]);
+  expect(fetch.mock.calls.length).toEqual(1);
+});*/

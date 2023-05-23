ad2d22ec9d47ef38b6b50545feee6f4c134e729a
Mason Schönherr <mascit43@hs-esslingen.de>
2023-05-23 20:39:55 +0200

    feat: AddBonus Seite hinzugefügt
    Ref: SCP-15
    Time: 5h

--- Frontend/src/components/AddBonus.jsx
+++ Frontend/src/components/AddBonus.jsx
@@ -30,12 +30,81 @@ class AddBonus extends Component{
         this.submitHandle = this.submitHandle.bind(this);
       }
 
-      componentDidMount(){
+      async componentDidMount(){
         console.log(this.props.params.action);
+        //Loads all the Bonuses and writes them in to the table below
+        let response = await fetch("http://localhost:8081/bonus/", { method: "GET", credentials: "include" });
+        let resData = await response.json();
+        this.setState({ allSport: resData });
       }
 
       submitHandle(event) {
 
+        const sportCheckboxEl = document.getElementsByClassName("form_sport_checkbox");
+        const dateOptions = { day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", minute: "2-digit" };
+        const infoContainerEl = document.getElementById("form_info_container");
+        const infoMessageEl = document.getElementById("form_info_message");
+
+        let startDate = new Date(this.state.bonusStartDate);
+        let endDate = new Date(this.state.bonusEndDate);
+        let startDateFormat = startDate.toLocaleDateString("de-GE", dateOptions).replace(" ", "");
+        let endDateFormat = endDate.toLocaleDateString("de-GE", dateOptions).replace(" ", "");
+        let sportCheckedId = [];
+        let bonusFactor = [];
+
+        infoContainerEl.classList.remove("error");
+        infoContainerEl.classList.remove("success");
+
+        if (this.state.bonusName === "") {
+          this.showInputErrorMessage("Bitte gebe deinem Bonus einen Namen!");
+          return;
+        }
+
+        //Creates the JSON object corresponding to the bonus object in the Backend
+        let bonusJsonObj = {};
+        bonusJsonObj.name = this.state.bonusName;
+        bonusJsonObj.description = this.state.bonusDescription;
+        bonusJsonObj.factor = this.state.bonusFactor;
+        bonusJsonObj.startDate = startDateFormat;
+        bonusJsonObj.endDate = endDateFormat;
+
+        //Checks if min one Challenge is checked
+        if (sportCheckedId.length === 0) {
+        this.showInputErrorMessage("Du musst mindestens eine Sportart für deinen Bonus auswählen!");
+        return;
+        }
+
+        //Creates body data for the fetch
+        let fetchBodyData = new FormData();
+        fetchBodyData.append("sportId", sportCheckedId);
+        fetchBodyData.append("bonusFactor", bonusFactor);
+        fetchBodyData.append("json", JSON.stringify(bonusJsonObj));
+
+        //Gives data to the Backend and writes it into the DB
+        fetch("http://localhost:8081/bonus/", { method: "POST", body: fetchBodyData, credentials: "include" })
+        .then((response) => {
+        if (response.ok) {
+          response.json().then((resData => {
+          infoContainerEl.classList.add("success");
+          infoMessageEl.innerHTML = "Der Bonus wurde erolgreich erstellt!";
+          window.scrollTo(0, 0);
+          this.clearAllInputs();
+          }));
+        } else {
+        this.showInputErrorMessage(
+          "Beim erstellen des Bonuses ist etwas schief gelaufen: " + response.status + " " + response.statusText + "!"
+        );
+          }
+        })
+        .catch((error) => {
+        this.showInputErrorMessage("Beim erstellen des Bonuses ist etwas schief gelaufen: " + error + "!");
+        });
+
+        for (let i = 0; i < sportCheckboxEl.length; i++) {
+          if (sportCheckboxEl[i].checked) {
+            sportCheckedId.push(sportCheckboxEl[i].dataset.sportId);
+          }
+        }
       }
 
       bonusNameChange(event) {
@@ -57,6 +126,15 @@ class AddBonus extends Component{
       }
       bonusEndDateChange(event) {
         this.setState({ bonusEndDate: event.target.value });
+      }
+
+      clearAllInputs(){
+
+        this.setState({ bonusName: "" });
+        this.setState({ bonusDescription: "" });
+        this.setState({ bonusFactor: 1 });
+        this.setState({ bonusStartDate: "" });
+        this.setState({ bonusEndDate: "" });
       }
 
     render() {
